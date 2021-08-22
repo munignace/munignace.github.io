42a1015b1ad4441f7aec4153586722c4b62b4667
munignace <munyarugendoignace9@gmail.com>
2021-08-22 08:55:36 +0000

    done

---
+++ quiz.js
@@ -0,0 +1,50 @@
+function check()
+{
+  var c=0;
+  var q1=document.quiz.question1.value;
+  var q2=document.quiz.question2.value;
+  var q3=document.quiz.question3.value;
+  var q4=document.quiz.question4.value;
+  var q5=document.quiz.question5.value;
+  var q6=document.quiz.question6.value;
+  var q7=document.quiz.question7.value;
+  var q8=document.quiz.question8.value;
+  var q9=document.quiz.question9.value;
+  var q10=document.quiz.question10.value;
+  var q11=document.quiz.question11.value;
+  var q12=document.quiz.question12.value;
+  var q13=document.quiz.question13.value;
+  var q14=document.quiz.question14.value;
+  var q15=document.quiz.question15.value;
+  var q16=document.quiz.question16.value;
+  var q17=document.quiz.question17.value;
+  var q18=document.quiz.question18.value;
+  var q19=document.quiz.question19.value;
+  var q20=document.quiz.question20.value;
+
+ 
+
+  if (q1=="Strongly Disagree") {c++}
+  if (q2=="Strongly Agree") {c++}
+  if (q3=="Strongly Disagree") {c++}
+  if (q4=="Strongly Disagree") {c++}
+  if (q5=="Strongly Agree") {c++}
+  if (q6=="Strongly Disagree") {c++}
+  if (q7=="Strongly Agree") {C++}
+  if (q8=="Strongly Agree") {c++}
+  if (q9=="Strongly Agree") {c++}
+  if (q10=="Strongly Disagree") {c++}
+  if (q11=="Strongly Disagree") {c++}
+  if (q12=="Strongly Agree") {c++}
+  if (q13=="Strongly Agree") {c++}
+  if (q14=="Strongly Disagree") {c++}
+  if (q15=="Strongly Agree") {c++}
+  if (q16=="Strongly Agree") {c++}
+  if (q17=="Strongly Disagree") {c++}
+  if (q18=="Strongly Disagree") {c++}
+  if (q19=="Strongly Agree") {c++}
+  if (q20=="Strongly Agree") {c++}
+  document.write("<h1 style='color:  blue;'>You got"+"\n"+c+"/20"+ "\n"+"Keep it up!<h1>");
+  document.write("<hr/>");
+  document.write("<img src='images.png'/>")
+}
